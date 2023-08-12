32436fa40a59c0432f3c231012f0a7de717c0543
BaptPicxDev <picard.baptiste@laposte.net>
2023-08-12 12:23:01 +0100

    Adding js swipper now swaps projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,20 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* Swiper */
+let swiperProjects = new Swiper(".projects__container", {
+    loop: true,
+    spaceBetween: 24,
+
+    cssMode: true,
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
